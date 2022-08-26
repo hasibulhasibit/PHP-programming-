5bc18978f77a61061c8679d0f3581457c3fe776e
hasibulhasibit <hasibulislam5088872@gmail.com>
2022-08-26 15:30:38 +0600

    Mathwithphp

---
+++ Basics of PHP/Mathwithphp.php
@@ -0,0 +1,27 @@
+<?php
+//Math with php 
+$number = 450;
+$number2 = $number - 145;
+$number = 25 * 5;
+$number = 15 / 5; // forward slash, Backward slash;
+$number = 15 % 2;
+$number = (50-15) * 6;
+echo $number, "\n";
+
+$number +=1; //number = $number + 1,
+
+//+ - * / % modules = Arithmetic operator,
+
+$number = 45;
+$member = ++$number;
+echo $member, "\n",$number;
+/*
+$member = $number++;
+$member =$number;
+$number = $number + 1;
+
+$member = ++$number;
+$number = $number + 1;
+$member =$number;
+*/
+
