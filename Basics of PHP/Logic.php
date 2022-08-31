3ab3afdb4566f99fb8dff03a7f59189bd2e57d22
hasibulhasibit <hasibulislam5088872@gmail.com>
2022-08-31 19:47:58 +0600

    Logic

--- Basics of PHP/Logic.php
+++ Basics of PHP/Logic.php
@@ -1,18 +1,20 @@
 <?php
-$n= 12;
+$n= 17;
 if ($n % 2== 0) {
     echo "$n is an even number";
 }else{
     echo "$n is a odd number";
 }
+echo "\n";
+
 if ($n > 20){
     echo"$n is greater then 20";
 } 
 // Php Logical operators.....
 /*
-exmple:
-== equle 
-!= not equle
+example:
+== equal 
+!= not equal
 >
 <
 >=
@@ -34,4 +36,17 @@ if( "hakim" == $food || "salmon" == $food){
 }
 else{
     echo "we don't Know if {$food} contains Vitamin A ";
+}
+echo "\n";
+
+$alam = 100;
+$rohim = 250;
+if ( $alam == $rohim){
+    echo "Alam and Rohim has same amount of money"; 
+}elseif ($alam>$rohim) {
+    echo"Alam has more money then rahim";
+}elseif ($alam<$rohim) {
+    echo"Alam has less money then rahim";
+}elseif ($alam>=$rohim) {
+    echo "Alam has same or more money than Rahim";
 }
