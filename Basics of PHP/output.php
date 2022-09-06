5d2e96f777f8f588387f0aee319e1d816d59abdf
hasibulhasibit <hasibulislam5088872@gmail.com>
2022-09-07 01:26:54 +0600

    output

---
+++ Basics of PHP/output.php
@@ -0,0 +1,24 @@
+<?php
+/* PHP Data Type 
+1.Integer
+2.Double/Float
+3.Boolean
+4.Null
+5.String
+6.Array
+7.Object
+8.Resource
+*/
+
+    $name= "Earth";
+    var_dump($name);
+    $age= "20";
+    var_dump($age);
+    $number= "125.0214";
+    var_dump($number);
+    $name1= "Hasibul";
+    $name2= "Islam";
+    var_dump($name1,$name2);
+    $result = true;
+    var_dump ($result);
+
