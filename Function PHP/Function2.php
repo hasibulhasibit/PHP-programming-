c4b098a990aac7d45f4dd24ba320efe80a8a5ce1
hasibulhasibit <hasibulislam5088872@gmail.com>
2022-09-24 00:11:19 +0600

    function.php

---
+++ Function PHP/Function2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*Determines if the argument is even or odd, 
+* you can hide function code use -" include_once "folder / file " .
+*/
+
+include_once "Functions.php";
+
+$x = 12;
+
+if (isEven($x)){//argument
+ echo " {$x} is an even number";
+} else {
+    echo"{$x} is an odd number";
+}
---
+++ Function PHP/Functions.php
@@ -0,0 +1,8 @@
+<?php
+function isEven ($n) // paremeter
+{
+    if ($n %2 == 0){
+        return true;
+    }
+    return false;
+}
