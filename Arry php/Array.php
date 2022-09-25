d04f1e0c4038d56368242cb31ea45011cf7e7866
hasibulhasibit <hasibulislam5088872@gmail.com>
2022-09-25 23:58:07 +0600

    Array

---
+++ Arry php/Array.php
@@ -0,0 +1,21 @@
+<?php
+$n = "12";
+// $students = array (
+//     "rahim",
+//     "abul",
+//     "kalam",
+// );
+
+$students =[    
+    "rahim",
+    "abul",
+    "kalam",
+];// JS Type array ,
+
+// array loop:
+
+$n = count ( $students);
+for ( $i = 0; $i < $n; $i++){
+    echo $students[$i] . "\n";
+}
+
